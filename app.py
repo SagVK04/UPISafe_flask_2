f683f8117cfc5dd8696c0dfc33416bc48a3bd4f3
Sagnik Metiya <metiyasagnik692@gmail.com>
2026-03-04 02:02:43 +0530

    NLP Done, ML Done, Flask Endpoint done

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
 import pickle
-from flask import Flask,jsonify,request
-import numpy as np
+import nltk
+from flask import Flask, jsonify, request
+import pandas as pd
 
-model_D_T = pickle.load(open('fraud_detector_D_T.pkl','rb'))
+model_rfc = pickle.load(open('fraud_detector.pkl', 'rb'))
+tr_label = pickle.load(open('transaction_label.pkl', 'rb'))
+
 app = Flask(__name__)
+
+
 @app.route('/', methods=['GET'])
 def home():
     print('Hi Welcome!')
@@ -14,77 +19,50 @@ def home():
         'Next_Actions': str("Get Data from Users")
     })
 
+
+def transform_text(text):
+    text = text.lower()
+    text = nltk.word_tokenize(text)
+    y = []
+    for i in text:
+        if i.isalnum():
+            y.append(i)
+    return "".join(y)
+
+
 @app.route('/', methods=['POST'])
 def predict():
-    print('Hello Coders!')
+    upi_id = request.form.get('upi_id')
+    date_input = request.form.get('date')  # DD/MM/YYYY
+    time_input = request.form.get('time')  # HH:MM
+    amount_input = request.form.get('amount')
+    type_input = request.form.get('type')  # Requested/Debited
 
-    in_vr = request.form.get('international')
-    fl_vr = request.form.get('fail')
-    ty_vr = request.form.get('type')
-    de_vr = request.form.get('device')
-    amount_vr = request.form.get('amount')
-    hour_vr = request.form.get('time')
+    upi_id_mod = transform_text(upi_id)
+    num_char = len(upi_id_mod)
+    amount = float(amount_input)
+    date = int(date_input.replace('/', ''))
+    time = int(time_input.replace(':', ''))
+    type = tr_label.transform([type_input])[0]
 
-    if in_vr.lower() == 'yes':
-        in_vr = 1
-    else:
-        in_vr = 0
+    input = pd.DataFrame({
+        'num_char_modified': [num_char], 'new_r_id': [upi_id_mod],
+        'Amount': [amount], 'Date_mod': [date],
+        'Time_mod': [time], 'Type_mod': [type]
+    })
+    pred_fin_rfc = model_rfc.predict(input)
+    pred_fin_score_rfc = model_rfc.predict_proba(input)
 
-    if fl_vr.lower() == 'yes':
-        fl_vr = 1
-    else:
-        fl_vr = 0
-
-    if ty_vr.lower() == 'online':
-        ty_vr = 0
-    elif ty_vr.lower() == 'pos':
-        ty_vr = 1
-    elif ty_vr.lower() == 'atm':
-        ty_vr = 2
-    elif ty_vr.lower() == 'transfer':
-        ty_vr = 3
-
-    if de_vr.lower() == 'tablet':
-        de_vr = 0
-    elif de_vr.lower() == 'computer':
-        de_vr = 1
-    elif de_vr.lower() == 'mobile':
-        de_vr = 2
-
-    if amount_vr == '' or amount_vr is None:
-        return jsonify({
-            'Value Error': str(1)
-        })
-    elif int(amount_vr) < 0:
-        return jsonify({
-            'Value Error': str(1)
-        })
-    else:
-        am_vr = int(amount_vr)
-
-    if hour_vr == '' or hour_vr is None:
-        return jsonify({
-            'Value Error': str(2)
-        })
-    elif int(hour_vr) < 0:
-        return jsonify({
-            'Value Error': str(3)
-        })
-    else:
-        hr_vr = int(hour_vr)
-
-    input = np.array([[am_vr,in_vr,ty_vr,fl_vr,de_vr,hr_vr]])
-    pred_D_T = model_D_T.predict(input)
-    pred_score_D_T = model_D_T.predict_proba(input)
-
-    fin_res_D_T = pred_D_T[0]
-    risk_score_D_T = pred_score_D_T[0][1]
-    print("Fraud_Result: ",fin_res_D_T==1)
-    print("Risk_Score: ",int(risk_score_D_T*100))
+    fin_res_rfc = pred_fin_rfc[0]
+    risk_score_rfc = pred_fin_score_rfc[0][1]
+    print("Fraud_Result: ", fin_res_rfc == 1)
+    print("Risk_Score: ", int(risk_score_rfc * 100))
 
     return jsonify({
-        'Fraud_Result': str(fin_res_D_T==1),
-        'Risk_Score': str(int(risk_score_D_T*100))
+        'Fraud_Result': str(fin_res_rfc == 1),
+        'Risk_Score': str(int(risk_score_rfc * 100))
     })
+
+
 if __name__ == "__main__":
     app.run()
